f2e1ff49a076cf73cabb6ae100f61c6495129c8f
Mandi Grant <mandi018@gmail.com>
2014-08-06 19:58:09 -0700

    Completed the How Not to Do It step

--- router.js
+++ router.js
@@ -1,20 +1,11 @@
 function route(handle, pathname){
   console.log("About to route a request for " + pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    return handle[pathname]();
   } else {
     console.log("no request handler found for " + pathname);
+    return "404 Not Found";
   }
 }
 
-function start() {
-  console.log("Request handler 'start' was called.");
-}
-
-function upload() {
-  console.log("Request handler 'upload' was called.");
-}
-
-exports.start = start;
-exports.upload = upload;
 exports.route = route;
